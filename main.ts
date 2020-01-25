faf8106fef635ecf826a3b1ceea0ca0d7fb1162e
Daniel Stewart <mpc823_99@yahoo.com>
2020-01-25 12:50:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,498 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 
+7 7 5 7 5 7 7 7 7 7 7 7 7 5 7 7 
+7 7 6 5 7 7 7 7 7 7 7 7 7 5 7 7 
+7 7 6 5 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 6 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 1 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 1 3 1 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 1 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 5 7 7 7 5 7 7 7 7 5 7 7 7 7 
+7 7 7 5 7 5 7 7 7 7 7 5 7 7 7 7 
+7 7 7 7 7 5 7 7 7 7 7 7 5 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+. d c d c d c d c d c d c d c d 
+. c d c d c d c d c d c d c d c 
+. d c d c d c d c d c d c d c d 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+b b b b b b b b b b b b b b b b 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+b b b b b b b b b b b b b b b b 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+c c c c c c c c c c c c c c c c 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+b b b b b b b b b b b b b b b b 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d d d c b b b b b b b b b b b b 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c b b b b b b b b b b b b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+d d d c b b b b b b b b b b b b 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c b b b b b b b b b b b b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+d d d c 2 2 2 b 2 2 2 2 2 2 2 b 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+d d d c b b b b b b b b b b b b 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+d d d c 2 2 2 2 2 2 2 b 2 2 2 2 
+c c c c 2 2 2 2 2 2 2 b 2 2 2 2 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+b b b b b b b b b b b b b b b b 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+2 2 2 b 2 2 2 2 2 2 2 b 2 2 2 2 
+b b b b b b b b b b b b b b b b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+2 2 2 2 2 2 2 b 2 2 2 2 2 2 2 b 
+`
+}
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, MyPlayer)
+    MyPlayer.setImage(img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f d 5 5 5 5 5 5 d f . . 
+. f d 5 5 5 5 5 5 5 5 d f . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+f b 5 5 5 5 5 5 5 5 5 5 b f 
+f b 5 5 5 5 5 5 5 5 5 5 b f 
+f b b 5 5 5 5 5 5 5 5 b b f 
+f b b b d 5 5 5 5 d b b b f 
+f b b b b b b b b b b b b f 
+f f b b b b b b b b b b f . 
+. c c b b b b b b b b c c . 
+. 4 d c f f f f f f c d 4 . 
+. 4 f b 3 b 3 b 3 b b f 4 . 
+. . f f 3 b 3 b 3 b f f . . 
+. . . . f f b b f f . . . . 
+`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    xDirection = -1
+    yDirection = 0
+    animation.runImageAnimation(
+    MyPlayer,
+    [img`
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 c . 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+f f f 4 4 c b c b 5 5 5 b f 
+. f f d d c 1 e b b b b b c 
+. . f d d d d 4 f b b b b c 
+. . f 4 4 4 e e e 4 b b c . 
+. . f 9 9 9 e d d 4 f f . . 
+. . f 9 9 9 e d d e f . . . 
+. f 3 3 b 3 b e e b f . . . 
+. f f 3 b 3 b 3 b f f . . . 
+. . . f f b b f f . . . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 5 c 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+. f f 4 4 c b c b 5 5 5 b f 
+. . f d d c 1 e b b b b b c 
+. . c d d d d e e b b b b c 
+. . c 4 4 4 e d d 4 b b c . 
+. . c 9 9 9 e d d e c c . . 
+. c b b b b b e e b b c . . 
+. c 3 b 3 b 3 3 3 b c . . . 
+. . c c c b b c c c . . . . 
+`],
+    100,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    yDirection = 1
+    xDirection = 0
+    animation.runImageAnimation(
+    MyPlayer,
+    [img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 d b b d 5 5 5 f . 
+f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+f 5 5 c c 4 4 4 4 c c 5 5 f 
+f b b f b f 4 4 f b f b b f 
+f b b 4 1 f d d f 1 4 b b f 
+. f b f d d d d d d f b f . 
+. f e f e 4 4 4 4 e f e f . 
+. e 4 f 6 9 9 9 9 6 f 4 e . 
+. 4 d c 9 9 9 9 9 9 c d 4 . 
+. 4 f b 3 b 3 b 3 b b f 4 . 
+. . f f 3 b 3 b 3 3 f f . . 
+. . . . f f b b f f . . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+c b 5 5 5 d b b d 5 5 5 b c 
+f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+f 5 5 c c 4 4 4 4 c c 5 5 f 
+f b b f b f 4 4 f b f b b f 
+f b b e 1 f d d f 1 e b b f 
+c f b f d d d d d 4 4 b f c 
+. c e c 6 9 9 9 4 d d d c . 
+. e 4 c 9 9 9 9 4 d d 4 c . 
+. e c b b 3 b b b e e c . . 
+. . c c 3 3 b 3 b 3 c c . . 
+. . . . c b b c c c . . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+c b 5 5 5 d b b d 5 5 5 b c 
+f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+f 5 5 c c 4 4 4 4 c c 5 5 f 
+f b b f b f 4 4 f b f b b f 
+f b b e 1 f d d f 1 e b b f 
+c f b 4 4 d d d d d f b f c 
+. c d d d 4 9 9 9 6 c e c . 
+. c 4 d d 4 9 9 9 9 c 4 e . 
+. . c e e b b b 3 b b c e . 
+. . c c 3 b 3 b 3 3 c c . . 
+. . . . c c c b b c . . . . 
+`],
+    100,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, MyPlayer)
+    MyPlayer.setImage(img`
+. . . f f f c c c . . . . . 
+. f f 5 5 5 5 5 5 f f . . . 
+. f 5 5 5 5 5 5 5 5 5 f . . 
+f 5 5 5 5 5 5 5 5 5 5 5 c . 
+f 5 5 b d 5 5 5 5 5 5 5 c . 
+f 5 d 4 4 b 5 5 5 5 5 5 5 f 
+f 5 b 4 4 4 c c 5 5 5 5 5 f 
+f f f 4 4 c b c b 5 5 5 b f 
+. f f d d c 1 e b b b b b c 
+. . f d d d d 4 f b b b b c 
+. . f 4 4 4 e e e 4 b b c . 
+. . f 9 9 9 e d d 4 f f . . 
+. . f 9 9 9 e d d e f . . . 
+. f 3 3 b 3 b e e b f . . . 
+. f f 3 b 3 b 3 b f f . . . 
+. . . f f b b f f . . . . . 
+`)
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, MyPlayer)
+    MyPlayer.setImage(img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 d b b d 5 5 5 f . 
+f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+f 5 5 c c 4 4 4 4 c c 5 5 f 
+f b b f b f 4 4 f b f b b f 
+f b b 4 1 f d d f 1 4 b b f 
+. f b f d d d d d d f b f . 
+. f e f e 4 4 4 4 e f e f . 
+. e 4 f 6 9 9 9 9 6 f 4 e . 
+. 4 d c 9 9 9 9 9 9 c d 4 . 
+. 4 f b 3 b 3 b 3 b b f 4 . 
+. . f f 3 b 3 b 3 3 f f . . 
+. . . . f f b b f f . . . . 
+`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 5 2 . . . . . . 
+. . . . . . 2 2 5 2 2 . . . . . 
+. . . . . . . 2 5 2 . . . . . . 
+. . . . . . . . 7 . . . . . . . 
+. . . . . . . . 7 7 . . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+. . . . . . . . 7 . . . . . . . 
+`, MyPlayer, 100 * xDirection, 100 * yDirection)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    xDirection = 1
+    yDirection = 0
+    animation.runImageAnimation(
+    MyPlayer,
+    [img`
+. . . . . c c c f f f . . . 
+. . . f f 5 5 5 5 5 5 f f . 
+. . f 5 5 5 5 5 5 5 5 5 f . 
+. c 5 5 5 5 5 5 5 5 5 5 5 f 
+. c 5 5 5 5 5 5 5 d b 5 5 f 
+f 5 5 5 5 5 5 5 b 4 4 d 5 f 
+f 5 5 5 5 5 c c 4 4 4 b 5 f 
+f b 5 5 5 b c b c 4 4 f f f 
+c b b b b b e 1 c d d f f . 
+c b b b b f 4 d d d d f . . 
+. c b b 4 e e e 4 4 4 f . . 
+. . f f 4 d d e 9 9 9 f . . 
+. . . f e d d e 9 9 9 f . . 
+. . . f b e e b 3 b 3 3 f . 
+. . . f f b 3 b 3 b 3 f f . 
+. . . . . f f b b f f . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . . . c c c f f f . . . 
+. . . f f 5 5 5 5 5 5 f f . 
+. . f 5 5 5 5 5 5 5 5 5 f . 
+. c 5 5 5 5 5 5 5 5 5 5 5 f 
+c 5 5 5 5 5 5 5 5 d b 5 5 f 
+f 5 5 5 5 5 5 5 b 4 4 d 5 f 
+f 5 5 5 5 5 c c 4 4 4 b 5 f 
+f b 5 5 5 b c b c 4 4 f f . 
+c b b b b b e 1 c d d f . . 
+c b b b b e e d d d d c . . 
+. c b b 4 d d e 4 4 4 c . . 
+. . c c e d d e 9 9 9 c . . 
+. . c b b e e b b b b b c . 
+. . . c b 3 3 3 b 3 b 3 c . 
+. . . . c c c b b c c c . . 
+`],
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, MyPlayer)
+    MyPlayer.setImage(img`
+. . . . . c c c f f f . . . 
+. . . f f 5 5 5 5 5 5 f f . 
+. . f 5 5 5 5 5 5 5 5 5 f . 
+. c 5 5 5 5 5 5 5 5 5 5 5 f 
+. c 5 5 5 5 5 5 5 d b 5 5 f 
+f 5 5 5 5 5 5 5 b 4 4 d 5 f 
+f 5 5 5 5 5 c c 4 4 4 b 5 f 
+f b 5 5 5 b c b c 4 4 f f f 
+c b b b b b e 1 c d d f f . 
+c b b b b f 4 d d d d f . . 
+. c b b 4 e e e 4 4 4 f . . 
+. . f f 4 d d e 9 9 9 f . . 
+. . . f e d d e 9 9 9 f . . 
+. . . f b e e b 3 b 3 3 f . 
+. . . f f b 3 b 3 b 3 f f . 
+. . . . . f f b b f f . . . 
+`)
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    yDirection = -1
+    xDirection = 0
+    animation.runImageAnimation(
+    MyPlayer,
+    [img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f d 5 5 5 5 5 5 d f . . 
+. f d 5 5 5 5 5 5 5 5 d f . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+f b 5 5 5 5 5 5 5 5 5 5 b f 
+f b 5 5 5 5 5 5 5 5 5 5 b f 
+f b b 5 5 5 5 5 5 5 5 b b f 
+f b b b d 5 5 5 5 d b b b f 
+f b b b b b b b b b b b b f 
+f f b b b b b b b b b b f . 
+. c c b b b b b b b b c c . 
+. 4 d c f f f f f f c d 4 . 
+. 4 f b 3 b 3 b 3 b b f 4 . 
+. . f f 3 b 3 b 3 b f f . . 
+. . . . f f b b f f . . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 b f . . 
+. f 5 5 5 5 5 5 5 5 5 b f . 
+f d 5 5 5 5 5 5 5 5 5 b f . 
+f d 5 5 5 5 5 5 5 5 d b b f 
+f b 5 5 5 5 5 5 5 5 b b b f 
+f b b 5 5 5 5 5 d b b b b f 
+f b b b b b b b b b b b b f 
+f b b b b b b b b b b b f c 
+f c b b b b b b b b b c c . 
+4 d 4 f f f f f f f c 4 c . 
+. 4 c b b 3 b 3 b 4 d d 4 . 
+. . c c 3 3 b 3 b b 4 e . . 
+. . . . c b b c c c . . . . 
+`,img`
+. . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f b 5 5 5 5 5 5 5 f . . 
+. f b 5 5 5 5 5 5 5 5 5 f . 
+. f b 5 5 5 5 5 5 5 5 5 d f 
+f b b d 5 5 5 5 5 5 5 5 d f 
+f b b b 5 5 5 5 5 5 5 5 b f 
+f b b b b d 5 5 5 5 5 b b f 
+f b b b b b b b b b b b b f 
+c f b b b b b b b b b b b f 
+. c c b b b b b b b b b c f 
+. c 4 c f f f f f f f 4 d 4 
+. 4 d d 4 b 3 b 3 b b c 4 . 
+. . e 4 b b 3 b 3 3 c c . . 
+. . . . c c c b b c . . . . 
+`],
+    100,
+    true
+    )
+})
+let projectile: Sprite = null
+let yDirection = 0
+let xDirection = 0
+let MyPlayer: Sprite = null
+MyPlayer = sprites.create(img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 d b b d 5 5 5 f . 
+f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+f 5 5 c c 4 4 4 4 c c 5 5 f 
+f b b f b f 4 4 f b f b b f 
+f b b 4 1 f d d f 1 4 b b f 
+. f b f d d d d d d f b f . 
+. f e f e 4 4 4 4 e f e f . 
+. e 4 f 6 9 9 9 9 6 f 4 e . 
+. 4 d c 9 9 9 9 9 9 c d 4 . 
+. 4 f b 3 b 3 b 3 b b f 4 . 
+. . f f 3 b 3 b 3 3 f f . . 
+. . . . f f b b f f . . . . 
+`, SpriteKind.Player)
+tiles.setTilemap(tiles.createTilemap(
+            hex`2000100002020203011414141414141414141418191a1b1c1d1e1f20211b1c1d30303030010303010204050505050505050506414241404140414041000031313131313002030201121212120a0a0a0a0a0a0745454545474748494900003131313131300103030212030a120a0a0a0a0a0a07000000001616161616161600000025003001020203120208120909090c0308090d0d0f0e1616161616161600000000003001030301120303120202121212124a4a4a4a4a4a35370035350035000000003001020202010201120303120303124a4a4a0f0f4a4a4a4a4a0000004c0000003001011501120202121212120303124a4a4a4a22334b4b4b4b4b4b4b4d000000270112121212010102020101030312104a4a4a223338383600000025282b2c2c2f4412010101030101010202030312104a4a4a223338383b3d3e3f25002a27002e0112030301010103030103030312104a344a223338383b3c2424000000292c2d0112010303370301020202130312104a344a223300003b3b24240000000013134412010202010301031501010312104a344a223300003a3a00000000390000130112121212121212121212120312134a4a4a4a22220000003e3f4040390000130103020103020303010303030312134a4a4a333333333333333333333333333301020102020301020302030303124a4a4a4a2323232323232323233232323232`,
+            img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath5,sprites.castle.tilePath4,sprites.castle.tilePath9,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,sprites.builtin.forestTiles0,sprites.builtin.forestTiles1,sprites.builtin.brick,sprites.castle.rock1,sprites.castle.saplingOak,sprites.castle.saplingPine,sprites.builtin.field0,sprites.dungeon.buttonOrange,sprites.builtin.crowd0,sprites.builtin.crowd1,sprites.builtin.crowd2,sprites.builtin.crowd3,sprites.builtin.crowd4,sprites.builtin.crowd5,sprites.builtin.crowd6,sprites.builtin.crowd7,sprites.builtin.crowd8,sprites.builtin.crowd9,sprites.dungeon.buttonOrangeDepressed,sprites.dungeon.hazardWater,sprites.dungeon.hazardLava0,sprites.dungeon.hazardSpike,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection1,sprites.vehicle.roadIntersection2,sprites.vehicle.roadTurn3,sprites.vehicle.roadIntersection3,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn4,sprites.vehicle.roadVertical,sprites.vehicle.roadIntersection4,sprites.dungeon.buttonPinkDepressed,sprites.builtin.field1,sprites.dungeon.hazardHole,sprites.castle.rock2,sprites.castle.shrub,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.chestOpen,sprites.dungeon.collectibleInsignia,sprites.dungeon.doorLockedEast,sprites.dungeon.floorDark3,sprites.dungeon.floorDark0,sprites.dungeon.floorDark5,sprites.dungeon.floorDark1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenSwitchDown,sprites.dungeon.chestClosed,myTiles.tile1,myTiles.tile2,sprites.dungeon.doorClosedEast,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.doorClosedWest,sprites.dungeon.floorMixed,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7],
+            TileScale.Sixteen
+        ))
+controller.moveSprite(MyPlayer, 50, 50)
+scene.cameraFollowSprite(MyPlayer)
+xDirection = 1
+yDirection = 0
